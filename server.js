e01216e8a38a2ca27c8057a85311362395b3fee8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-25 00:54:34 +0900

    dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
@@ -17,6 +18,8 @@ const server = new ApolloServer({
   resolvers,
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
