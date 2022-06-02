77386b6ab48994c999a6a73525b36b3a610af04c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-02 20:22:29 +0900

    enable playground

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return { loggedInUser: await getUser(req.headers.token) };
   },
