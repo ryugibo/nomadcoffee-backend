bc329fe67c526b39f6fd0b3654b839c6279de099
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-23 04:08:27 +0900

    add some include

--- src/coffeeShops/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeShops/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -3,11 +3,13 @@ import client from "../../client";
 export default {
   Query: {
     seeCoffeeShops: async (_, { page }) => {
-      return client.coffeeShop.findMany({
+      const data = await client.coffeeShop.findMany({
         orderBy: { id: "asc" },
         take: 5,
         skip: 5 * (page - 1),
+        include: { user: true, photos: true, categories: true },
       });
+      return data;
     },
   },
 };
